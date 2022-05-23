174af70ff426a2bf88f7dd5666f616ae2507eb67
Koteterov <koteterov@yahoo.com>
2022-05-23 09:49:55 +0300

    factory function

---
+++ factoryFuncHTMLElements.js
@@ -0,0 +1,13 @@
+function el(type, attr, ...content) {
+    const element = document.createElement(type);
+    for (let prop in attr) {
+      element[prop] = attr[prop];
+    }
+    for (let item of content) {
+      if (typeof item == "string" || typeof item == "number") {
+        item = document.createTextNode(item);
+      }
+      element.appendChild(item);
+    }
+    return element;
+  }
